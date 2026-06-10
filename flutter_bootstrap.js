2d2f6128d223d4317e9e7ff8102a0c27f4dcf4ef
largevillachongyang-a11y <largevillachongyang-a11y@users.noreply.github.com>
2026-06-10 02:36:18 +0000

    deploy: 5ccd68917d40d413007756176a06cb5cb38fd345 5ccd68917d40d413007756176a06cb5cb38fd345

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "916837005" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1032118976" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
